9fc51b0868405d00565223ac146258320c31843f
AlejandroPineapple <alejandro_pina@anahuac.mx>
2024-10-11 21:07:03 -0600

    ya se va viendo algo

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StatusBar, View, Text, Image } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from './screens/Home'
+
+const Stack = createStackNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  return(
+    <>
+      <StatusBar 
+        barStyle = {'light-content'}
+        backgroundColor={"#121212"}
+      />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName = "Home">
+          <Stack.Screen
+            name = "Home"
+            component = { Home }
+            options= {{
+              headerLeft: () => (
+                <View style = {{flexDirection: 'row', alignItems: 'center'}}>
+                  <Image
+                    style = {{width: 40, height: 40, marginRight: 10, marginLeft: 10}}
+                    source = {{uri: 'https://www.clipartmax.com/png/middle/236-2361875_pusheen-aka-the-cute-of-cuteness-by-favouritefi-cute-cat-drawing-png.png'}}
+                  />
+                  <Text style = {{color: 'white', fontSize: 18}}> Pito </Text>
+                </View>
+              ),
+              headerTitleAlign: 'center',
+              headerStyle: {
+                backgroundColor: "#121212"
+              },
+              headerTintColor: '#fff'
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  )
+}
---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
+
+export default function Home(){
+    return (
+        <>
+            <SafeAreaView>
+                <Text> Hola xdxd </Text>
+            </SafeAreaView>
+        </>
+    )
+}
