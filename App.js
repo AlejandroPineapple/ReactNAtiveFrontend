fdba0d9bbac1f214bae90dcb8ee12a11592735b8
AlejandroPineapple <alejandro_pina@anahuac.mx>
2024-10-15 20:23:13 -0600

    flexes

--- App.js
+++ App.js
@@ -1,10 +1,65 @@
 import React from 'react'
-import { StatusBar, View, Text, Image } from 'react-native'
+import { StatusBar, View, Text, Image, TouchableOpacity } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './screens/Home'
 
 const Stack = createStackNavigator()
+
+function MyTabBar({ state, descriptors, navigation }) {
+  return (
+    <View style={{ flexDirection: 'row' , alignItems: 'center', backgroundColor: '#121212', height: 40, paddingBottom: 10}}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+              ? options.title
+              : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Text style={{marginLeft: 10, color: isFocused ? '#ffffff' : '#ff0000' }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return(
@@ -15,7 +70,6 @@ export default function App() {
       />
 
       <NavigationContainer>
-        <Stack.Navigator initialRouteName = "Home">
           <Stack.Screen
             name = "Home"
             component = { Home }
@@ -36,7 +90,9 @@ export default function App() {
               headerTintColor: '#fff'
             }}
           />
-        </Stack.Navigator>
+        <Tab.Navigator tabBar={(props) => <MyTabBar {...props} />}>
+          <Tab.Screen name="Home" component={Home} />
+        </Tab.Navigator>
       </NavigationContainer>
     </>
   )
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,44 @@
 import React from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function Home(){
     return (
         <>
-            <SafeAreaView>
-                <Text> Hola xdxd </Text>
-            </SafeAreaView>
+            <View 
+            style={{
+                backgroundColor: "#121212",
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+
+                <View 
+                style={{
+                    backgroundColor: "#420039",
+                    width: "90%",
+                    height: "40%",
+                    borderRadius: 20
+                }}>
+                </View>
+                <View 
+                style={{
+                    backgroundColor: "#12355b",
+                    width: "90%",
+                    height: "15%",
+                    borderRadius: 20,
+                }}>
+                </View>
+                <View 
+                style={{
+                    backgroundColor: "#d72638",
+                    width: "90%",
+                    height: "40%",
+                    borderRadius: 20
+                }}>
+                </View>
+
+            </View>
         </>
     )
 }
